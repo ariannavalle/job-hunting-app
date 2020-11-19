d5e09fa0e6f4b9fc7936f0ed3df437060d63252f
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-11-18 20:15:41 -0500

    build folder

--- server/app.js
+++ server/app.js
@@ -46,6 +46,10 @@ app.use('/', require('./routes/column.routes'));
 // app.use((req, res, next) => {
 //   next(createError(404));
 // });
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // catch all error handler
 app.use((error, req, res, next) => {
